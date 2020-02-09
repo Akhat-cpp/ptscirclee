a0e39d36fd0826571c5ff7abcf36d6edcbcc4d65
Akhat-cpp <56387863+Akhat-cpp@users.noreply.github.com>
2020-02-09 19:35:53 +0200

    Add files via upload

---
+++ circle (1).js
@@ -0,0 +1,42 @@
+// Source code licensed under Apache License 2.0.
+// Copyright © 2017 William Ngan. (https://github.com/williamngan/pts)
+ 
+window.demoDescription = "A circle and a donut meets. Indicate their points of intersections.";
+ 
+//// Demo code starts (anonymous function wrapper is optional) ---
+ 
+(function() {
+ 
+  // Pts quick start mode.
+  var run = Pts.quickStart( "#pt", "#fe3" );
+  let xy = space.center;
+  run( (time, ftime) => {
+   
+    let c1 = Circle.fromCenter( space.pointer, space.size.y/4 );
+    let c2 = Circle.fromCenter( space.pointer, space.size.y/8 );
+    let ct = Circle.fromCenter( space.center, space.size.y/4 );
+ 
+    let ins1 = Circle.intersectCircle2D( c1, ct );
+    let ins2 = Circle.intersectCircle2D( c2, ct );
+    
+    if(ins2.length > 0)
+    {
+        c1 =  Math.sqrt(add(multiply(subtract(space.center.x, xy.x), subtract(space.center.x, xy.x)) , multiply((subtract(space.center.y, xy.y), subtract(space.center.y, xy.y)))))
+        c2 =  Math.sqrt(add(multiply(subtract(space.center.x, xy.x), subtract(space.center.x, xy.x)) , multiply((subtract(space.center.y, xy.y), subtract(space.center.y, xy.y)))))
+    }
+    if(ins2.length == 0)
+    {
+        c1 = Circle.fromCenter( space.pointer, space.size.y/4);
+        c2 = Circle.fromCenter( space.pointer, space.size.y/8);
+        xy = space.pointer;
+    }
+ 
+    form.fillOnly( "#0c6" ).circle( c1 );
+    form.fill( "#fe3" ).circle( c2 );
+    form.fill( "rgba(70,30,240,.2)" ).circle( ct );
+    form.fill( "rgba(70,30,240, .3)" ).points( ins1, 10, "circle" );
+    form.fill( "#f06" ).points( ins2, 5, "circle" );
+ 
+  });
+ 
+})();
